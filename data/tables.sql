ead30dbc7089dedab39853d3159c09a6606a86a2
Ikechukwu Okalia <dev@ikechukwuokalia.com>
2020-12-18 21:34:30 +0100

    Working update

--- data/tables.sql
+++ data/tables.sql
@@ -17,6 +17,8 @@ CREATE TABLE `wallet_history` (
   `id` int(11) NOT NULL,
   `user` char(32) NOT NULL,
   `type` char(16) NOT NULL,
+  `amount` DECIMAL(18,6) NOT NULL,
+  `new_balance` DECIMAL(18,6) NOT NULL,
   `narration` text NOT NULL,
   `_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- src/Wallet.php
+++ src/Wallet.php
@@ -1,9 +1,11 @@
 <?php
 namespace SOS;
+require_once __DIR__ . "/Eml-Temp.php";
 use \TymFrontiers\Data,
     \TymFrontiers\MultiForm,
     \TymFrontiers\Validator,
     \TymFrontiers\Generic,
+    \TymFrontiers\MySQLDatabase,
     \TymFrontiers\InstanceError;
 
 class Wallet{
@@ -29,21 +31,110 @@ class Wallet{
   public $balance;
 
   protected $_updated;
+  protected $_conn = false;
+
+  public $errors = [];
 
   function __construct(string $user, string $currency) {
-    if (
-      $user = (new Validator)->username($user, ["user","username",3,32,[],"UPPER",["-",".","_"]])
-      && $currency = (new Validator)->username($currency, ["currency","username",3,5])
-    ) {
-      $this->_init($user, $currency);
-      var_dump("valid");
+    $gen = new Generic;
+    $arg = $gen->requestParam([
+      "user" => ["user","username",3,32,[],"UPPER",["-",".","_"]],
+      "currency" => ["currency","username",3,5]
+    ],[
+      "user" => $user,
+      "currency" => $currency
+    ], ["user","currency"]);
+    if ($arg && empty($gen->errors)) {
+      $this->_init($arg["user"], $arg["currency"]);
     }
   }
-  public function credit(string $user, string $currency, float $amount, string $naration, string $alert_email = "") {
-
+  public function credit(float $amount, string $narration, ? string $alert_email = null) {
+    // save narration
+    if (!$this->_conn) $this->_conn = new MySQLDatabase(MYSQL_SERVER, MYSQL_DEVELOPER_USERNAME, MYSQL_DEVELOPER_PASS);
+    $data = new Data;
+    if (!empty($this->user)) {
+      $this->balance += $amount;
+      $query  = "UPDATE `".self::$_db_name."`.`".self::$_table_name."` ";
+      $query .= "SET `balance` = '{$this->_conn->escapeValue($data->encodeEncrypt($this->balance))}' ";
+      $query .= "WHERE `user` = '{$this->_conn->escapeValue($this->user)}' ";
+      $query .= "AND `currency` = '{$this->_conn->escapeValue($this->currency)}' LIMIT 1";
+      if ($this->_conn->query($query)) {
+        if (!empty($this->_conn->errors["query"])) unset($this->_conn->errors["query"]);
+        $inset = "INSERT INTO `".self::$_db_name."`.`wallet_history` (`user`, `type`, `amount`, `new_balance`, `narration`) VALUES (
+          '{$this->_conn->escapeValue($this->user)}',
+          'CREDIT',
+          {$amount},
+          {$this->balance},
+          '{$this->_conn->escapeValue($narration)}'
+        )";
+        if (!$this->_conn->query($inset)) {
+          if ($na_errs = (new InstanceError($this->_conn,true))->get("query")) {
+            foreach ($na_errs as $err) {
+              $this->errors["credit"][] = $err;
+            }
+          }
+          $this->errors["credit"][] = [1,256,"Failed to create wallet history.",__FILE__,__LINE__];
+          if (!empty($this->_conn->errors["query"])) unset($this->_conn->errors["query"]);
+          return false;
+        }
+        if (!empty($alert_email)) $this->_queue_alert($amount, $alert_email, $narration, "CREDIT");
+        return true;
+      } else {
+        $this->errors["credit"][] = [0,256,"Failed to credit wallet.",__FILE__,__LINE__];
+        $this->balance -= $amount;
+        if (!empty($this->_conn->errors["query"])) {
+          $q_errors = (new InstanceError($this->_conn,true))->get("query");
+          foreach($q_errors as $qe) {
+            $this->errors["credit"][] = $qe;
+          }
+        }
+      }
+    }
+    return false;
   }
-  public function debit(string $user, string $currency, float $amount, string $naration, string $alert_email = "") {
-
+  public function debit(float $amount, string $narration, ? string $alert_email = null) {
+    // save narration
+    if (!$this->_conn) $this->_conn = new MySQLDatabase(MYSQL_SERVER, MYSQL_DEVELOPER_USERNAME, MYSQL_DEVELOPER_PASS);
+    $data = new Data;
+    if (!empty($this->user)) {
+      $this->balance -= $amount;
+      $query  = "UPDATE `".self::$_db_name."`.`".self::$_table_name."` ";
+      $query .= "SET `balance` = '{$this->_conn->escapeValue($data->encodeEncrypt($this->balance))}' ";
+      $query .= "WHERE `user` = '{$this->_conn->escapeValue($this->user)}' ";
+      $query .= "AND `currency` = '{$this->_conn->escapeValue($this->currency)}' LIMIT 1";
+      if ($this->_conn->query($query)) {
+        if (!empty($this->_conn->errors["query"])) unset($this->_conn->errors["query"]);
+        $inset = "INSERT INTO `".self::$_db_name."`.`wallet_history` (`user`, `type`, `amount`, `new_balance`, `narration`) VALUES (
+          '{$this->_conn->escapeValue($this->user)}',
+          'DEBIT',
+          {$amount},
+          {$this->balance},
+          '{$this->_conn->escapeValue($narration)}'
+        )";
+        if (!$this->_conn->query($inset)) {
+          if ($na_errs = (new InstanceError($this->_conn,true))->get("query")) {
+            foreach ($na_errs as $err) {
+              $this->errors["credit"][] = $err;
+            }
+          }
+          $this->errors["debit"][] = [1,256,"Failed to create wallet history.",__FILE__,__LINE__];
+          if (!empty($this->_conn->errors["query"])) unset($this->_conn->errors["query"]);
+          return false;
+        }
+        if (!empty($alert_email)) $this->_queue_alert($amount, $alert_email, $narration, "DEBIT");
+        return true;
+      } else {
+        $this->errors["debit"][] = [0,256,"Failed to debit wallet.",__FILE__,__LINE__];
+        $this->balance -= $amount;
+        if (!empty($this->_conn->errors["query"])) {
+          $q_errors = (new InstanceError($this->_conn,true))->get("query");
+          foreach($q_errors as $qe) {
+            $this->errors["debit"][] = $qe;
+          }
+        }
+      }
+    }
+    return false;
   }
   private function _init (string $user, string $currency) {
     self::_checkEnv();
@@ -51,13 +142,16 @@ class Wallet{
     if ($found = self::findBySql("SELECT * FROM :db:.:tbl: WHERE `user`='{$database->escapeValue($user)}' AND `currency`='{$database->escapeValue($currency)}' LIMIT 1")) {
       $this->_load($found[0]);
     } else {
-      echo $database->last_query;
       $this->_register($user, $currency);
     }
   }
-  private function _load(self $user) {
-    echo "loading";
-    var_dump($user);
+  private function _load(? self $user) {
+    if ($user instanceof self) {
+      foreach ($user as $key => $value) {
+        if (\property_exists($this, $key)) $this->$key = $value;
+      }
+      $this->balance = (float) (new Data)->decodeDecrypt($this->balance);
+    }
   }
   private function _register(string $user, string $currency) {
     global $database;
@@ -67,8 +161,7 @@ class Wallet{
       "user" => ["user","username",3,32,[],"UPPER",["-",".","_"]],
       "currency" => ["currency","username", 3, 6]
     ], ["user"=>$user, "currency"=>$currency], ["user","currency"]);
-    echo "called:";
-    if (!$params || !empty($gen->errors)) {
+    if (!$params && !empty($gen->errors)) {
       $errors = (new InstanceError($gen,true))->get("requestParam",true);
       throw new \Exception("Invalid string parsed for user/currency: ".\implode(" | ", $errors), 1);
     }
@@ -78,8 +171,51 @@ class Wallet{
     if (!$this->_create()) {
       throw new \Exception("Failed to register Wallet user", 1);
     }
+    $this->balance = 0.00;
   }
-  private function _queue_alert (string $user, string $naration) {
+  private function _queue_alert (float $amount, string $email, string $narration, string $type = "CREDIT") {
+    $email_prop = Generic::splitEmailName($email);
+    if (empty($email_prop["email"])) throw new \Exception("Invalid/empty email parsed as argument.", 1);
+    global $email_replace_pattern, $alert_eml;
+    if (empty($email_replace_pattern) || \is_array($email_replace_pattern)) {
+      $email_replace_pattern = [
+        "name" => "%name%",
+        "surname" => "%surname%",
+        "email" => "%email%",
+        "type" => "%type%",
+        "currency" => "%currency%",
+        "amount" => "%amount%",
+        "new_balance" => "%new_balance%",
+        "date" => "%date%"
+      ];
+      $replace_val = [
+        "name" => $email_prop["name"],
+        "surname" => "",
+        "email" => $email_prop["email"],
+        "type" => $type,
+        "date" => \strftime(\TymFrontiers\BetaTym::MYSQL_DATETYM_STRING,\time()),
+        "currency" => $this->currency,
+        "amount" => \number_format($amount, (\in_array($this->currency, ["NGN","USD","GBP","EUR"]) ? 2 :8), ".", ","),
+        "new_balance" => \number_format($this->balance, (\in_array($this->currency, ["NGN","USD","GBP","EUR"]) ? 2 :8), ".", ",")
+      ];
+      $message = $alert_eml;
+      foreach ($replace_val as $prop=>$value) {
+        $message = \str_replace($email_replace_pattern[$prop], $value, $message);
+      }
+      $subject = "New [{$type}] transaction on your {$this->currency} Wallet";
+      $msg_text = "Debit of {$replace_val["currency"]} {$replace_val["amount"]} occured on your {$this->currency} Wallet";
+      $receiver = empty($email_prop["name"])
+        ? $email_prop["email"]
+        : "{$email_prop["name"]} <{$email_prop["email"]}>";
+      $queue = new EMailer([
+        "sender" => PRJ_AUTO_EMAIL,
+        "receiver" => $receiver,
+        "subject" => $subject,
+        "msg_html" => $message,
+        "msg_text" => $msg_text,
+      ],1);
+      $queue->queue(1);
+    }
 
   }
   // check environment
@@ -90,9 +226,11 @@ class Wallet{
         !\defined("MYSQL_BASE_DB") ||
         !\defined("MYSQL_SERVER") ||
         !\defined("MYSQL_GUEST_USERNAME") ||
-        !\defined("MYSQL_GUEST_PASS")
+        !\defined("MYSQL_GUEST_PASS") ||
+        !\defined("MYSQL_DEVELOPER_USERNAME") ||
+        !\defined("MYSQL_DEVELOPER_PASS")
       ){
-        throw new \Exception("Required defination(s)[MYSQL_BASE_DB, MYSQL_SERVER, MYSQL_GUEST_USERNAME, MYSQL_GUEST_PASS] not [correctly] defined.", 1);
+        throw new \Exception("Required defination(s)[MYSQL_BASE_DB, MYSQL_SERVER, MYSQL_GUEST_USERNAME, MYSQL_GUEST_PASS, MYSQL_DEVELOPER_USERNAME, MYSQL_DEVELOPER_PASS] not [correctly] defined.", 1);
       }
       // check if guest is logged in
       $GLOBALS['database'] = new \TymFrontiers\MySQLDatabase(MYSQL_SERVER, MYSQL_GUEST_USERNAME, MYSQL_GUEST_PASS);
